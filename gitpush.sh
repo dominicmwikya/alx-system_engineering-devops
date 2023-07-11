9f554c03cbd3ce19da76ef037d2bcacb9ecb323c
dominicmwikya <dominicmwikya50@gmail.com>
2023-07-11 19:47:29 +0000

    Update code

---
+++ gitpush.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Change to the directory containing your Git repository
+cd /alx-system_engineering-devops
+
+# Add all modified and new files to the staging area
+git add .
+
+# Commit the changes with a commit message
+commit_message="Update code"
+git commit -m "$commit_message"
+
+# Push the changes to the remote repository
+git push origin master
